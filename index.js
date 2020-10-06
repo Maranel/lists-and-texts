46a1b1e33559786b340dd8bc8ee2215e80495d84
Maranel <50551100+Maranel@users.noreply.github.com>
2020-10-06 20:55:26 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 
 
-let array = ['John','Matus','Petra'];
+let array = ['John','Matus','Petra', 'Martina'];
 
 array.forEach(element =>{
-    $('#last').append('<li>',element,'</li>')
+    $('ul').append('<li>' + element + '</li>');
     console.log(element);
 }   );
+
+let additionalBlock = {
+    title: "Added with javascript",
+    text: "This block was added using JavaScript's jQuery library. How awesome!"
+  };
+
+
+  $(document).ready(function() {
+    document.title = additionalBlock.title;
+});
+
+  console.log(additionalBlock.title);
+  $(".end").append("<p>" + additionalBlock.text + "</p>");
